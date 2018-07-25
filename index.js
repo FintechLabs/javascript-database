8f69312093429ed9f2a2911a9f7fc2b5c26780f4
Hiten Pratap Singh <hitenpratap99@gmail.com>
2018-07-25 14:30:01 +0530

    Made DB config dynamic

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 var mysql = require("mysql");
 
-var pool = mysql.createPool({
+var databaseConfigMap = {};
+databaseConfigMap["development"] = {
   connectionLimit: 10,
   host: "",
-  user: "root",
+  user: "",
   password: "",
   port: "",
   database: ""
-});
+};
+
+var pool = mysql.createPool(databaseConfigMap["development"]);
 
 var resMap = {};
 
 pool.query(
-  "SELECT loan_application_status,count(*) FROM loan_application INNER JOIN tenant on tenant.id=loan_application.tenant_id where secondary_domain='client2.lendingmantrasandbox.com' group by loan_application_status",
+  "SELECT loan_application_status,count(*) FROM loan_application INNER JOIN tenant on tenant.id=loan_application.tenant_id where domain='dev1.p2pforce.com' group by loan_application_status",
   function(error, result) {
     if (error) {
       console.log(error);
     } else {
       resMap = result;
       console.log("The solution is: ", result);
-      console.log(JSON.stringify(resMap));
     }
   }
 );
